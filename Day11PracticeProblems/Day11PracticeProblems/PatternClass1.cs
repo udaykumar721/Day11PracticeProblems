b4a03a0f18a72161755f1072844e273511ae8a55
udaykumar721 <uday56367@gmail.com.com>
2023-09-25 15:19:44 +0530

    [UDAY]add : InvertedRightTriangleStarPattern

---
+++ Day11PracticeProblems/Day11PracticeProblems/PatternClass1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11PracticeProblems
+{
+    internal class PatternClass1
+    {
+        public void  Pattren(int input)
+        {
+            for(int i = 0; i < input; i++)
+            {
+                for(int j = input; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Day11PracticeProblems/Day11PracticeProblems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PatternClass1 pat = new PatternClass1();
+            Console.WriteLine("Enter The Number Of Rows You Want");
+            int input = Convert.ToInt32(Console.ReadLine());
+            pat.Pattren(input);
+            Console.ReadLine();
+        }
+    }
+}
